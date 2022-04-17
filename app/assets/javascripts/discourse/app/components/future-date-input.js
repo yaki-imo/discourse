6fa1f501f5025b4c3cb95a5021471abafea99f41
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-04-17 20:24:25 +0400

    No need to pass statusType because we don't use it in calculations

--- app/assets/javascripts/discourse/app/components/future-date-input.js
+++ app/assets/javascripts/discourse/app/components/future-date-input.js
@@ -4,6 +4,7 @@ import { and, empty, equal } from "@ember/object/computed";
 import buildTimeframes from "discourse/lib/timeframes-builder";
 import I18n from "I18n";
 import { FORMAT } from "select-kit/components/future-date-input-selector";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   selection: null,
@@ -24,7 +25,7 @@ export default Component.extend({
 
     if (this.input) {
       const dateTime = moment(this.input);
-      const closestTimeframe = this.findClosestTimeframe(dateTime);
+      const closestTimeframe = this._findClosestTimeframe(dateTime);
       if (closestTimeframe) {
         this.set("selection", closestTimeframe.id);
       } else {
@@ -43,6 +44,26 @@ export default Component.extend({
     if (this.label) {
       this.set("displayLabel", I18n.t(this.label));
     }
+  },
+
+  @discourseComputed
+  shortcuts() {
+    const opts = {
+      includeWeekend: this.includeWeekend,
+      includeFarFuture: this.includeFarFuture,
+      includeDateTime: this.includeDateTime,
+      canScheduleNow: this.includeNow || false,
+    };
+
+    return buildTimeframes(this.userTimezone, opts).map((tf) => {
+      return {
+        id: tf.id,
+        name: I18n.t(tf.label),
+        time: tf.time,
+        timeFormatted: tf.timeFormatted,
+        icon: tf.icons,
+      };
+    });
   },
 
   @action
@@ -73,7 +94,7 @@ export default Component.extend({
     }
   },
 
-  findClosestTimeframe(dateTime) {
+  _findClosestTimeframe(dateTime) {
     const options = {
       includeWeekend: this.includeWeekend,
       includeFarFuture: this.includeFarFuture,
