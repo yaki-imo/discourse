84af0da49500267ecf7324ee3958f083abe4d929
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-04-21 20:23:44 +0400

    Finish tests for drying up of options hiding

--- app/assets/javascripts/discourse/tests/unit/lib/time-shortcut-test.js
+++ app/assets/javascripts/discourse/tests/unit/lib/time-shortcut-test.js
@@ -1,6 +1,9 @@
 import { module, test } from "qunit";
 import { fakeTime } from "discourse/tests/helpers/qunit-helpers";
-import buildTimeframes from "discourse/lib/timeframes-builder";
+import {
+  defaultTimeShortcuts,
+  hideDynamicTimeShortcuts,
+} from "discourse/lib/time-shortcut";
 
 module(
   "Unit | Lib | time-shortcut | hideDynamicTimeShortcuts",
@@ -13,29 +16,76 @@ module(
 
     test("hides 'Later Today' at the end of the day", function (assert) {
       const timezone = moment.tz.guess();
-      this.clock = fakeTime("2100-04-19 18:00:00", timezone, true); // Monday evening
-      const timeframes = buildTimeframes(timezone).mapBy("id");
+      const shortcuts = defaultTimeShortcuts(timezone);
 
-      assert.notOk(timeframes.includes("later_today"));
-      assert.ok(timeframes.includes("later_this_week"));
+      this.clock = fakeTime("2100-04-19 08:00:00", timezone, true); // morning
+      let result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.ok(
+        result.includes("later_today"),
+        "shows later_today in the morning"
+      );
+
+      this.clock = fakeTime("2100-04-19 18:00:00", timezone, true); // evening
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(result.includes("doesn't show later_today in the evening"));
     });
 
     test("hides 'Later This Week' starting from Thursday", function (assert) {
       const timezone = moment.tz.guess();
-      this.clock = fakeTime("2100-04-22 18:00:00", timezone, true); // Thursday evening
-      const timeframes = buildTimeframes(timezone).mapBy("id");
+      const shortcuts = defaultTimeShortcuts(timezone);
 
-      assert.notOk(timeframes.includes("later_this_week"));
+      this.clock = fakeTime("2100-04-21 18:00:00", timezone, true); // Wednesday
+      let result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.ok(
+        result.includes("later_this_week"),
+        "shows later_this_week on Wednesdays"
+      );
+
+      this.clock = fakeTime("2100-04-22 18:00:00", timezone, true); // Thursday
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(
+        result.includes("later_this_week"),
+        "doesn't show later_this_week on Thursdays"
+      );
+
+      this.clock = fakeTime("2100-04-23 18:00:00", timezone, true); // Friday
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(
+        result.includes("later_this_week"),
+        "doesn't show later_this_week on Fridays"
+      );
     });
 
     test("hides 'This Weekend' on Fridays, Saturdays and Sundays", function (assert) {
       const timezone = moment.tz.guess();
-      this.clock = fakeTime("2100-04-23 18:00:00", timezone, true); // Friday
+      const shortcuts = defaultTimeShortcuts(timezone);
 
+      this.clock = fakeTime("2100-04-22 18:00:00", timezone, true); // Thursday
+      let result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
       assert.ok(
-        !buildTimeframes(timezone, { includeWeekend: true })
-          .mapBy("id")
-          .includes("this_weekend")
+        result.includes("this_weekend"),
+        "shows this_weekend on Thursdays"
+      );
+
+      this.clock = fakeTime("2100-04-23 18:00:00", timezone, true); // Friday
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(
+        result.includes("this_weekend"),
+        "doesn't show this_weekend on Fridays"
+      );
+
+      this.clock = fakeTime("2100-04-24 18:00:00", timezone, true); // Saturday
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(
+        result.includes("this_weekend"),
+        "doesn't show this_weekend on Saturdays"
+      );
+
+      this.clock = fakeTime("2100-04-25 18:00:00", timezone, true); // Sunday
+      result = hideDynamicTimeShortcuts(shortcuts, timezone).mapBy("id");
+      assert.notOk(
+        result.includes("this_weekend"),
+        "doesn't show this_weekend on Sundays"
       );
     });
   }
